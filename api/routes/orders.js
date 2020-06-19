e24f04fff39d69a16882d82fee8cbc28a0394e7a
cloudnhs <cloudnhs@gmail.com>
2020-06-19 17:59:57 +0900

    server.js

---
+++ api/routes/orders.js
@@ -0,0 +1,4 @@
+const express = require('express'); //express 모듈 연동
+const router = express.Router(); //라우터 함수 묶음 정의
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const orderRouter = require('./api/routes/orders');
+const productRouter = require('./api/routes/products');
+
+// dB 연결
+const dbAddress = "mongodb+srv://test:UVgYhxt5L4qU5jMB@cluster0-2ksti.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose
+    .connect(dbAddress, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MONGODB connected..."))
+    .catch(err => console.log(err.message));
+
+//미들웨어
+
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/order', orderRouter);
+app.use('/product', productRouter);
+
+const PORT = 6000;
+
+app.listen(PORT, console.log('server start'));
